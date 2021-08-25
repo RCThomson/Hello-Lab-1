f322b2c22c5a4a59ddfff139e8f9ebc3382d56e0
RCThomson <46541337+RCThomson@users.noreply.github.com>
2021-08-25 12:21:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***************************************************************************
+* Hello.java
+****************************************************************************
+* Rebecca Thomson
+* 8/25/2021
+* CMSC 255 004
+****************************************************************************/
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Rebecca Thomson");
+		System.out.println("Hello.java");
+		System.out.println("Hello.java says Hello!");
+		System.out.println("8/25/2021");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+	}
+}
